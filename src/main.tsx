9f592d777d351f7e76074fba030092ec763b2b99
Evan Freymiller <evanfreymiller@MacBookPro.localdomain>
2024-12-04 12:36:35 -0500

    removed auth from code

--- src/main.tsx
+++ src/main.tsx
@@ -13,15 +13,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCa1_NijEOfeNeSVfxhTkMnpAA0P_29bPo",
-  authDomain: "litebritepro.firebaseapp.com",
-  databaseURL: "https://litebritepro-default-rtdb.firebaseio.com",
-  projectId: "litebritepro",
-  storageBucket: "litebritepro.firebasestorage.app",
-  messagingSenderId: "504582650378",
-  appId: "1:504582650378:web:d3f8d73662fcf8f9c5fdf3",
-  measurementId: "G-TG5EZ58K1Y"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT
 };
 
 // Initialize Firebase
