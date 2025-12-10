ff2575629b2d74b0989043a44895502413ee936b
Evan Freymiller <evanfreymiller@MacBookPro.localdomain>
2025-12-09 21:56:11 -0500

    centered bottom nav

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -10,19 +10,19 @@ import {
   Text,
   useTheme,
   Box,
-  Flex,
   HStack,
   IconButton,
   Input,
   Tooltip,
   Badge,
+  Grid,
+  GridItem,
   // Slide,
 } from "@chakra-ui/react";
 import { VscDebugStart } from "react-icons/vsc";
 import { LuUnplug } from "react-icons/lu";
 import { PiPlugsConnectedFill } from "react-icons/pi";
 import { RiPaletteFill } from "react-icons/ri";
-import { TiMinus, TiPlus } from "react-icons/ti";
 import { useDeviceSize } from "../../hooks/useDeviceSize";
 import { IoTrashSharp } from "react-icons/io5";
 import { Logo } from "../../components/Logo";
@@ -31,7 +31,6 @@ const socket = io(window.location.origin);
 
 const StartSession = ({ handleCreateSession }: any) => (
   <>
-    <Box marginLeft="10px" />
     <Text>
       <HStack>
         <Tooltip hasArrow label="Start">
@@ -46,7 +45,6 @@ const StartSession = ({ handleCreateSession }: any) => (
         </Tooltip>
       </HStack>
     </Text>
-    <Box marginRight="5px" />
   </>
 );
 
@@ -276,93 +274,74 @@ function Root() {
           ))}
         </div>
       ))}
-      <Box
+      <Grid
         position="fixed"
-        bottom="0"
-        display="flex"
-        flexDir="row"
-        minW="100%"
-        padding="2"
-        minH="5vh"
         backgroundColor={theme.colors?.[palette || "gray"]["900"]}
-        alignItems="center"
+        bottom="0"
+        templateColumns="1fr 1fr 1fr"
+        gap={1}
+        minW="100%"
       >
-        <Logo palette={palette} />
-        <Flex flex={1} />
-        {isDesktop && (
-          <>
-            <Tooltip label="Decrease Scale" hasArrow>
-              <IconButton
-                borderRadius="100px"
-                icon={<TiMinus />}
-                marginRight="5px"
-                aria-label={"palette"}
-                onClick={() => setScale(scale - 1)}
-              />
-            </Tooltip>
-            <Tooltip label="Increase Scale" hasArrow>
-              <IconButton
-                borderRadius="100px"
-                icon={<TiPlus />}
-                marginRight="5px"
-                aria-label={"palette"}
-                onClick={() => setScale(scale + 1)}
-              />
-            </Tooltip>
-          </>
-        )}
-        <Tooltip label="Clear board" hasArrow>
-          <IconButton
-            borderRadius="100px"
-            icon={<IoTrashSharp />}
-            marginRight="5px"
-            aria-label={"palette"}
-            onClick={handleClearBoard}
-          />
-        </Tooltip>
-        <Tooltip label="Color Palette" hasArrow>
-          <IconButton
-            borderRadius="100px"
-            icon={<RiPaletteFill />}
-            marginRight="5px"
-            aria-label={"palette"}
-            onClick={handlePaletteChange}
-          />
-        </Tooltip>
-        <Flex flex={1} />
-
-        <Badge
-          colorScheme={theme.colors?.[palette || "gray"]["600"]}
-          display="flex"
-          flexDir="row"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Box margin="2">
-            {isConnected ? (
-              <PiPlugsConnectedFill
+        <GridItem>
+          <Logo palette={palette} />
+        </GridItem>
+        <GridItem display="flex" alignItems="center" justifyContent="center">
+          <HStack gap="1">
+            <Badge
+              colorScheme={theme.colors?.[palette || "gray"]["600"]}
+              display="flex"
+              flexDir="row"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Box margin="2">
+                {isConnected ? (
+                  <PiPlugsConnectedFill
+                    color={theme.colors?.[palette || "gray"]["300"]}
+                  />
+                ) : (
+                  <LuUnplug color={theme.colors?.[palette || "gray"]["300"]} />
+                )}
+              </Box>
+              <Text
+                marginRight="20px"
                 color={theme.colors?.[palette || "gray"]["300"]}
-              />
-            ) : (
-              <LuUnplug color={theme.colors?.[palette || "gray"]["300"]} />
+              >
+                {isConnected ? `Coop` : "Solo"}
+              </Text>
+            </Badge>
+            {!sessionId && (
+              <StartSession handleCreateSession={handleCreateSession} />
             )}
-          </Box>
-          <Text
-            marginRight="20px"
-            color={theme.colors?.[palette || "gray"]["300"]}
-          >
-            {isConnected ? `Coop` : "Solo"}
-          </Text>
-        </Badge>
-        {!sessionId && (
-          <StartSession handleCreateSession={handleCreateSession} />
-        )}
-        <ActiveSession
-          sessionIdInput={sessionIdInput}
-          handleInputChange={handleInputChange}
-          palette={palette}
-        />
-      </Box>
+            <ActiveSession
+              sessionIdInput={sessionIdInput}
+              handleInputChange={handleInputChange}
+              palette={palette}
+            />
+            <Tooltip label="Clear board" hasArrow>
+              <IconButton
+                borderRadius="100px"
+                icon={<IoTrashSharp />}
+                aria-label={"palette"}
+                onClick={handleClearBoard}
+              />
+            </Tooltip>
+            <Tooltip label="Color Palette" hasArrow>
+              <IconButton
+                borderRadius="100px"
+                icon={<RiPaletteFill />}
+                aria-label={"palette"}
+                onClick={handlePaletteChange}
+              />
+            </Tooltip>
+          </HStack>
+        </GridItem>
+        <GridItem
+          display="flex"
+          alignItems="center"
+          justifyContent="flex-end"
+        ></GridItem>
+      </Grid>
     </div>
   );
 }
